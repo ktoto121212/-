fdd6df253f8de1a87bfb2525a4a047cbf1d93ba9
ktoto121212 <148541971+ktoto121212@users.noreply.github.com>
2023-12-18 09:39:04 +0300

    Add files via upload

---
+++ калькулятор.py
@@ -0,0 +1,24 @@
+while True:                   #Кутышев Кулаков
+    try:
+        num1=float(input("1 число"))
+        de=input("действие +, -, :, *")
+        num2=float(input("2 число"))
+    except:
+        print("ошибка")
+    else:
+        if de=="+":
+            result=num1+num2
+            print(f"результат равен: {result}")
+            break
+        elif de=="-":
+            result=num1-num2
+            print(f"результат равен: {result}")
+            break
+        elif de=="*":
+            result=num1*num2
+            print(f"результат равен: {result}")
+            break
+        elif de==":":
+            result=num1/num2
+            print(f"результат равен: {result}")
+            break
