d5a3d6fcca19594c416672cacb56343141478b29
ktoto121212 <148541971+ktoto121212@users.noreply.github.com>
2023-12-19 10:27:50 +0300

    Update крутая регистрация.py

--- крутая регистрация.py
+++ крутая регистрация.py
@@ -18,15 +18,17 @@ while True:            #Кулаков
             except:
                 print("неверный логин")
             else:
-                if action == 2:                                   #вот тут добавь проверку пароля
-                    with open(file_name, "a") as f:
-                        write = input("введи пароль: ")
-                        if write in fin.read():
+                if action == 2:
+                    with open("akk.txt", "r") as f:
+                        login = input("Введите логин: ")
+                        f.read()
+                        if f.read == login:
                             print("пароль верный")
                             print("успешный вход в аккаунт")
                             break
                         else:
-                            print("неверный пароль, попробуйте снова")
+                            with open("akk.txt", "a") as f:
+                                print("неверный пароль")
                 elif action == 3:
                     with open(file_name, "w") as f:
                         write = input("введи новый пароль: ")
