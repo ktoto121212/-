33d29af57c6eb051866cabc6399145170efb8a21
ktoto121212 <148541971+ktoto121212@users.noreply.github.com>
2023-12-19 09:21:57 +0300

    Add files via upload

---
+++ крутая регистрация.py
@@ -0,0 +1,31 @@
+while True:            #Кулаков
+    try:
+        action = int(input("действия на сайте: 1 - создать новый акк, 2 - зайти в акк, 3 - сменить пароль"))
+    except ValueError:
+        print("такой команды нет")
+    else:
+        if action == 1:
+            file_name = input("введи новый логин: ")
+            with open(file_name, "x") as f:
+                write = input("введи пароль: ")
+                f.write(write)
+                break
+        while True:
+            try:
+                file_name = input("введи логин существующего аккаунта: ")
+                with open(file_name, "r") as f:
+                    f.read()
+            except:
+                print("неверный логин")
+            else:
+                if action == 2:                                   #вот тут добавь проверку пароля
+                    with open(file_name, "a") as f:
+                        write = input("добавь это в файл: ")
+                        f.write(write)
+                        break                                      #вот тут конец
+                elif action == 3:
+                    with open(file_name, "w") as f:
+                        write = input("введи новый пароль: ")
+                        f.write(write)
+                        break
+        break
